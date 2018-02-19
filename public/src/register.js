e709997c3a2b7cf3b655ac5167c1eb18ca1160c5
Michele Mirandola <miralo.web@gmail.com>
2018-02-19 10:35:44 +0100

    Add registration

--- public/src/register.js
+++ public/src/register.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails, CognitoIdentityCredentials, WebIdentityCredentials } from 'amazon-cognito-identity-js';
+
+const REGION = //Your Region Here
+const USER_POOL_ID = //Your USER POOL ID Here
+const CLIENT_ID = //Your CLIENT ID Here
+AWS.config.update({
+	region: REGION
+})
+
+const poolData = {
+	UserPoolId : USER_POOL_ID,
+	ClientId : CLIENT_ID
+}
+const userPool = new CognitoUserPool(poolData);
+
+
+class RegisterForm extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			name: '',
+			email: '',
+			password: '', 
+		}
+
+		this.RegisterUser = this.RegisterUser.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	RegisterUser(event) {
+
+		var attributeList = [];
+ 
+		var dataName = {
+			Name : 'name',
+			Value : this.state.name
+		};
+		var attributeName = 
+		new CognitoUserAttribute(dataName);
+		
+		attributeList.push(attributeName);
+		
+		var cognitoUser;
+		userPool.signUp(this.state.email, this.state.password, attributeList, null, function(err, result){
+			if (err) {
+				toastr.error(err);
+				return;
+			}
+			cognitoUser = result.user;
+			toastr.success('Complimenti ' + cognitoUser.getUsername() + ', ti sei registrato correttamente!');
+		});
+	}
+
 	render() {
 		return (
-		<div>
-			Hello {this.props.name}
-		</div>
+			<form className="ui form">
+				<div className="field">
+					<label>Nome e Cognome</label>
+					<input type="text" name="name" placeholder="es: Jon Snow" value={this.state.name} onChange={this.handleChange} />
+				</div>
+				<div className="field">
+					<label>Email</label>
+					<input type="text" name="email" placeholder="es: jon.snow@nightwatch.barrier"  value={this.state.email} onChange={this.handleChange} />
+				</div>
+				<div className="field">
+					<label>Password</label>
+					<input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+				</div>
+				<div className="field">
+					<div className="ui checkbox">
+						<input type="checkbox" tabIndex="0" className="hidden" />
+						<label>I agree to the Terms and Conditions</label>
+					</div>
+				</div>
+				<button className="ui green button" type="button" onClick={this.RegisterUser}>Registrati</button>
+			</form>
 		);
 	}
 }
   
 ReactDOM.render(
-	<HelloMessage name="Taylor" />,
-	document.getElementById('my-app')
+	<RegisterForm />,
+	document.getElementById('register')
 );
