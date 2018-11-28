68f4766e5cf04a486c8e354f2294d754fda94a25
Michele Mirandola <miralo.web@gmail.com>
2018-11-28 16:43:01 +0100

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ var MinifyPlugin = require("babel-minify-webpack-plugin");
 module.exports = {
 	entry: {
 		shared: ['babel-polyfill', './public/src/shared.js'],
-		register: ['babel-polyfill', './public/src/pages/register.js'],
-		login: ['babel-polyfill', './public/src/pages/login.js'],
-		index: ['babel-polyfill', './public/src/pages/index.js'],
-		dashboard: ['babel-polyfill', './public/src/pages/dashboard.js'],
-		projects: ['babel-polyfill', './public/src/pages/projects.js'],
-		single_project: ['babel-polyfill', './public/src/pages/single_project.js'],
+		register: ['./public/src/pages/register.js'],
+		login: ['./public/src/pages/login.js'],
+		index: ['./public/src/pages/index.js'],
+		dashboard: ['./public/src/pages/dashboard.js'],
+		projects: ['./public/src/pages/projects.js'],
+		single_project: ['./public/src/pages/single_project.js'],
 	},
 	output: {
 		path: path.resolve(__dirname, './public/dist'),
@@ -19,18 +19,12 @@ module.exports = {
 	},
 	devtool: 'cheap-source-map',
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
 				exclude: /node_modules/	
 			}
 		]
-	},
-	plugins: [
-		//new MinifyPlugin(),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'common' // Specify the common bundle's name.
-		})
-	]
+	}
 };
